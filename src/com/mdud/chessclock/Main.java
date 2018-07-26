d1d7a45f9de057a5368369ee8393db5cc25fbf39
MDud <duda915@gmail.com>
2018-07-27 00:16:07 +0200

    Fixed title and gravit design

--- src/com/mdud/chessclock/Main.java
+++ src/com/mdud/chessclock/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Chess clock");
         primaryStage.setScene(new Scene(root, 640, 360));
         primaryStage.setResizable(true);
         primaryStage.show();
